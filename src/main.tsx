09a76d7a3f8e3971cdd8c469c04ae077f08e8d45
UMER MUKHTAR <shah.umer8493@gmail.com>
2024-05-20 17:05:14 +0530

    store persist

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,18 @@ import './css/style.css';
 import './css/satoshi.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import {persistor,store} from "./redux/Store"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
       <App />
+      </Provider>
+    </PersistGate>
     </Router>
   </React.StrictMode>,
 );
