fe20fc9b039cc028e4ea373c10e984f8a2db932d
salman <najarsalman4@gmail.com>
2024-05-20 15:53:54 +0530

    add project

---
+++ src/App.tsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import Loader from './common/Loader/index.js';
+import PageTitle from './components/PageTitle.js';
+import SignIn from './pages/Authentication/SignIn.js';
+import SignUp from './pages/Authentication/SignUp.js';
+import Calendar from './pages/Calendar.js';
+import Chart from './pages/Chart.js';
+import ECommerce from './pages/Dashboard/ECommerce.js';
+import Material from './components/Material/Material.jsx';
+import FormLayout from './pages/Form/FormLayout.js';
+import Profile from './pages/Profile.js';
+import Settings from './pages/Settings.js';
+import Tables from './pages/Tables.js';
+import Alerts from './pages/UiElements/Alerts.js';
+import Buttons from './pages/UiElements/Buttons.js';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="Dashboard" />
+              <ECommerce />
+            </>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <>
+              <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Calendar />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Profile />
+            </>
+          }
+        />
+        <Route
+          path="/material/addMaterial"
+          element={
+            <>
+              <PageTitle title="Add Material" />
+              <Material />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-layout"
+          element={
+            <>
+              <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <FormLayout />
+            </>
+          }
+        />
+        <Route
+          path="/tables"
+          element={
+            <>
+              <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Tables />
+            </>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Settings />
+            </>
+          }
+        />
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Chart />
+            </>
+          }
+        />
+        <Route
+          path="/ui/alerts"
+          element={
+            <>
+              <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Alerts />
+            </>
+          }
+        />
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Buttons />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignIn />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignUp />
+            </>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Material/Material.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import DefaultLayout from '../../layout/DefaultLayout'
+
+const Material = () => {
+    return (
+
+        <DefaultLayout>
+            <div>
+                Add Material Form
+                <form action="" >
+                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" />
+                </form>
+            </div>
+
+        </DefaultLayout>
+    )
+}
+
+export default Material
