656690ace34f2f4ae6f1f6074a231a1a3eb73698
Jazim Nazir <109747177+Jazimnazir@users.noreply.github.com>
2024-05-23 11:20:47 +0530

    Changes

--- src/components/Material/Material.jsx
+++ src/components/Material/Material.jsx
@@ -1,132 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DefaultLayout from '../../layout/DefaultLayout';
-import { useFormik } from 'formik';
+import { ErrorMessage, Formik, useFormik } from 'formik';
 import * as Yup from 'yup';
+import ReactSelect from 'react-select';
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 
-const Material = () => {
-  const formik = useFormik({
-    initialValues: {
-      field1: '',
-      field2: '',
-      field3: '',
-      field4: '',
-      field5: '',
-    },
-    validationSchema: Yup.object({
-      field1: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-      field2: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-     
-    }),
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+const AddProduct = () => {
+    const [selectedOption, setSelectedOption] = useState(null);
 
-  return (
-    
-    <DefaultLayout>
-        <h1 className="text-3xl font-bold mb-4 text-black ">Material / Add Material </h1>
-          {/* <Breadcrumb pageName="Mterial / AddMaterial" /> */}
-      <div className="p-6 bg-white shadow-md rounded-md">
-      <h2 className="text-2xl font-bold mb-4 text-center">Add Material Form</h2>
+    const productgrp = [
+        { value: 'BrandA', label: 'Brand A' },
+        { value: 'BrandB', label: 'Brand B' },
+        { value: 'BrandC', label: 'Brand C' },
+    ];
 
-        <form onSubmit={formik.handleSubmit} className="space-y-4">
-          <div className="flex flex-wrap -mx-2">
-          <div className="w-full md:w-1/2 px-2 mb-4">
-  <label htmlFor="field1" className="block text-sm font-medium text-gray-700">
-    Field 1
-  </label>
-  <input
-    type="text"
-    id="field1"
-    name="field1"
-    placeholder="Field 1"
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    value={formik.values.field1}
-    className="mt-1 block w-full p-4 text-lg border border-gray-300 rounded-md"
-  />
-  {formik.touched.field1 && formik.errors.field1 ? (
-    <div className="text-red-600 text-sm">{formik.errors.field1}</div>
-  ) : null}
-</div>
+    const customStyles = {
+        control: (provided) => ({
+            ...provided,
+            minHeight: '50px', // Increase height of the control
+            fontSize: '16px', // Increase font size
+        }),
+        valueContainer: (provided) => ({
+            ...provided,
+            padding: '10px 14px', // Increase padding inside the control
+        }),
+        input: (provided) => ({
+            ...provided,
+            fontSize: '16px', // Increase font size for input
+        }),
+        singleValue: (provided) => ({
+            ...provided,
+            fontSize: '16px', // Increase font size for selected value
+        }),
+    };
 
-            <div className="w-full md:w-1/2 px-2 mb-4">
-              <label htmlFor="field2" className="block text-sm font-medium text-gray-700">
-                Field 2
-              </label>
-              <input
-                type="text"
-                id="field2"
-                name="field2"
-                placeholder="Field 2"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.field2}
-                className="mt-1 block w-full p-4 border border-gray-300 rounded-md"
-              />
-              {formik.touched.field2 && formik.errors.field2 ? (
-                <div className="text-red-600 text-sm">{formik.errors.field2}</div>
-              ) : null}
+    const formik = useFormik({
+        initialValues: {
+            unit: '',
+            price: '',
+            productDescription: '',
+            grade: '',
+        },
+        validationSchema: Yup.object({
+            unit: Yup.string().required('Required'),
+            price: Yup.string().required('Required'),
+            productDescription: Yup.string().required('Required'),
+            grade: Yup.string().required('Required'),
+        }),
+        onSubmit: (values) => {
+            console.log(values);
+        },
+    });
+
+    return (
+        <DefaultLayout>
+            <Breadcrumb pageName="Material / Add Material" />
+            <div>
+                <div className="flex flex-col gap-9">
+                    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                        <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+                            <h3 className="font-medium text-slate-500 text-center text-xl dark:text-white">
+                                Add Material
+                            </h3>
+                        </div>
+                        <form onSubmit={formik.handleSubmit} className="space-y-4">
+                            <div className="p-6.5">
+                                <div className="mb-6">
+                                    <label className="mb-2.5 block text-black dark:text-white">Product Description</label>
+                                    <textarea
+                                        rows={3}  // Reduced the number of rows for smaller height
+                                        name="productDescription"
+                                        placeholder="Type your message"
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.productDescription}
+                                        className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                                    ></textarea>
+                                    {formik.touched.productDescription && formik.errors.productDescription ? (
+                                        <div className="text-red-600 text-sm">{formik.errors.productDescription}</div>
+                                    ) : null}
+                                </div>
+
+                                <div className="mb-4.5 flex flex-wrap gap-6">
+                                    <div className="flex-1 min-w-[300px]">
+                                        <label className="mb-2.5 block text-black dark:text-white">Unit</label>
+                                        <ReactSelect
+                                            name="unit"
+                                            value={productgrp.find(option => option.value === formik.values.unit)}
+                                            onChange={(option) => formik.setFieldValue('unit', option.value)}
+                                            onBlur={formik.handleBlur}
+                                            options={productgrp}
+                                            styles={customStyles}
+                                            className="bg-white dark:bg-form-input"
+                                            classNamePrefix="react-select"
+                                            placeholder="Unit"
+                                        />
+                                        {formik.touched.unit && formik.errors.unit ? (
+                                            <div className="text-red-600 text-sm">{formik.errors.unit}</div>
+                                        ) : null}
+                                    </div>
+                                    <div className="flex-1 min-w-[300px]">
+                                        <label className="mb-2.5 block text-black dark:text-white">Price</label>
+                                        <input
+                                            type="text"
+                                            name="price"
+                                            placeholder="Price"
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            value={formik.values.price}
+                                            className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                                        />
+                                        {formik.touched.price && formik.errors.price ? (
+                                            <div className="text-red-600 text-sm">{formik.errors.price}</div>
+                                        ) : null}
+                                    </div>
+                                </div>
+
+                                <div className="mb-4.5 flex flex-wrap gap-6">
+                                    <div className="flex-1 min-w-[300px]">
+                                        <label className="mb-2.5 block text-black dark:text-white">Grade</label>
+                                        <input
+                                            type="text"
+                                            name="grade"
+                                            placeholder="Grade"
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            value={formik.values.grade}
+                                            className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                                        />
+                                        {formik.touched.grade && formik.errors.grade ? (
+                                            <div className="text-red-600 text-sm">{formik.errors.grade}</div>
+                                        ) : null}
+                                    </div>
+                                </div>
+
+                                <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-4">
+                                    Add Product
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="flex flex-wrap -mx-2">
-            <div className="w-full md:w-1/2 px-2 mb-4">
-              <label htmlFor="field3" className="block text-sm font-medium text-gray-700">
-                Field 3
-              </label>
-              <input
-                type="text"
-                id="field3"
-                name="field3"
-                placeholder="Field 3"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.field3}
-                className="mt-1 block w-full p-4 border border-gray-300 rounded-md"
-              />
-            </div>
-            <div className="w-full md:w-1/2 px-2 mb-4">
-              <label htmlFor="field4" className="block text-sm font-medium text-gray-700">
-                Field 4
-              </label>
-              <input
-                type="text"
-                id="field4"
-                name="field4"
-                placeholder="Field 4"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.field4}
-                className="mt-1 block w-full p-4 border border-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="field5" className="block text-sm font-medium text-gray-700">
-              Field 5
-            </label>
-            <input
-              type="text"
-              id="field5"
-              name="field5"
-              placeholder="Field 5"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.field5}
-              className="mt-1 block w-full p-4 border border-gray-300 rounded-md"
-            />
-          </div>
-          {/* <button type="submit" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md">
-            Submit
-          </button> */}
-           <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-4">
-                Submit
-            </button>
-        </form>
-      </div>
-    </DefaultLayout>
-  );
+        </DefaultLayout>
+    );
 };
 
-export default Material;
+export default AddProduct;
