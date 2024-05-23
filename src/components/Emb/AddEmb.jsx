7ff752066191195e851e11ee78bec317e6da2aef
UMER MUKHTAR <shah.umer8493@gmail.com>
2024-05-23 10:32:42 +0530

    sidebar

---
+++ src/components/Emb/AddEmb.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddEmb = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddEmb
---
+++ src/components/Emb/ViewEmb.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewEmb = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewEmb
