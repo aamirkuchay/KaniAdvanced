487bedde9c336fe9a5e6f84908edf3539df9abdb
UMER MUKHTAR <shah.umer8493@gmail.com>
2024-05-28 16:54:07 +0530

    MATERIAL

--- src/components/PuchaseOrder/MaterialPo.jsx
+++ src/components/PuchaseOrder/MaterialPo.jsx
@@ -186,7 +186,7 @@ const MaterialPo = () => {
                                 <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
                                     <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
                                         <h3 className="font-medium text-slate-500 text-center text-xl dark:text-white">
-                                            Add Material PO
+                                            ADD PURCHASE ORDER
                                         </h3>
                                     </div>
                                     <div className="p-6.5">
