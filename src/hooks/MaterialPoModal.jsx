ee6eb359e442910529ea9a01690fa2ed57429054
salman <najarsalman4@gmail.com>
2024-05-31 12:55:18 +0530

    new Modal

--- src/hooks/MaterialPoModal.jsx
+++ src/hooks/MaterialPoModal.jsx
@@ -4,11 +4,11 @@ const MaterialPoModal = ({ show, onClose, materialPos }) => {
     if (!show) return null;
 
     return (
-        <div className="fixed z-50 inset-0 overflow-y-auto flex items-center md:mt-12 md:ml-[190px] justify-center">
+        <div className="fixed z-9999 inset-0 overflow-y-auto flex items-center justify-center ">
             <div className="fixed inset-0 bg-gray-600 bg-opacity-75" onClick={onClose}></div>
-            <div className="bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:max-w-[600px] sm:ml-4 z-50">
-                <div className=" py-5 sm:px-6 ">
-                    <h3 className="text-lg leading-6 font-bold text-gray-900 text-center bg-slate-100 h-12 dark:bg-slate-600 ">Material POs</h3>
+            <div className="bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-3xl w-full sm:ml-4 z-50">
+                <div className="py-5 sm:px-6">
+                    <h3 className="text-lg py-2 leading-6 font-bold text-gray-900 text-center bg-slate-100 h-12 dark:bg-slate-600">Material POs</h3>
                 </div>
                 <div className="px-4 py-5 sm:px-6 overflow-y-auto max-h-80">
                     <div className="overflow-x-auto">
