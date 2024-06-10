e8fc6cac41e335263a637e1960743a7e3ce1e117
salman <najarsalman4@gmail.com>
2024-06-10 12:10:26 +0530

    color

--- src/components/Configurator/ViewTable.jsx
+++ src/components/Configurator/ViewTable.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 
-const ViewTable = ({ title, units, totalItems, handleDelete, handleUpdate,pagination,searchvalue }) => {
+const ViewTable = ({ title, units, totalItems, handleDelete, handleUpdate, pagination, searchvalue }) => {
     if (totalItems < 1) return (<><hr className='text-slate-300' /><p className='text-slate-400 text-2xl text-center py-5'>No {title} Available</p></>);
 
     // Function to generate table headers dynamically
-    console.log(pagination.itemsPerPage + 1,"heyyy");
+    console.log(pagination.itemsPerPage + 1, "heyyy");
     const startingSerialNumber = (pagination.currentPage - 1) * pagination.itemsPerPage + 1;
     const renderTableHeaders = () => {
 
@@ -35,7 +35,7 @@ const ViewTable = ({ title, units, totalItems, handleDelete, handleUpdate,pagina
 
         if (!units || units.length === 0) return null;
         return units.map((item, rowIndex) => {
-            const updatedItem = { ...item, id:startingSerialNumber + rowIndex }; // Update id value with index + 1
+            const updatedItem = { ...item, id: startingSerialNumber + rowIndex }; // Update id value with index + 1
             return (
                 <tr key={rowIndex} className='bg-white dark:bg-slate-700 dark:text-white'>
                     {Object.values(updatedItem).map((value, colIndex) => (
@@ -45,8 +45,8 @@ const ViewTable = ({ title, units, totalItems, handleDelete, handleUpdate,pagina
                     ))}
                     <td key={Object.keys(updatedItem).length} className="px-5 py-5 border-b border-gray-200 text-sm">
                         <p className="flex text-gray-900 whitespace-no-wrap">
-                            <FiEdit size={17} className='text-teal-500 hover:text-teal-700 mx-2' onClick={(e) => handleUpdate(e, item)} title='Edit Unit' />  |
-                            <FiTrash2 size={17} className='text-red-500 hover:text-red-700 mx-2' onClick={(e) => handleDelete(e, item.id)} title='Delete Unit' />
+                            <FiEdit size={17} className='text-teal-500 hover:text-teal-700 mx-2' onClick={(e) => handleUpdate(e, item)} title={`Edit ${title}`} />  |
+                            <FiTrash2 size={17} className='text-red-500 hover:text-red-700 mx-2' onClick={(e) => handleDelete(e, item.id)} title={`Delete  ${title}`} />
                         </p>
                     </td>
                 </tr>
@@ -58,7 +58,7 @@ const ViewTable = ({ title, units, totalItems, handleDelete, handleUpdate,pagina
     return (
         <div className="container mx-auto px-4 sm:px-8">
 
-                    
+
             <div className="pt-5">
                 <div className='flex justify-between'>
                     <h2 className="text-2xl font-semibold leading-tight text-center">View {title} </h2>
