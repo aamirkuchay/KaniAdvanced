3b7cacbf953dcd9c7fa4f856b4e31d3b283269b0
UMER MUKHTAR <shah.umer8493@gmail.com>
2024-06-24 14:27:10 +0530

    material search

--- src/Constants/utils.js
+++ src/Constants/utils.js
@@ -35,7 +35,7 @@ export const DELETE_MATERIAL_URL = `${BASE_URL}/material/delete/`;
 
 export const ADD_MATERIALPO_URL = `${BASE_URL}/purchaseOrder`;
 export const GET_MATERIALPO_BY_ID_URL = `${BASE_URL}/purchaseOrder`;
-export const GET_MATERIALPO_URL = `${BASE_URL}/purchaseOrder`;
+export const GET_MATERIALPO_URL = `${BASE_URL}/purchaseOrder/search`;
 export const UPDATE_MATERIALPO_URL = `${BASE_URL}/purchaseOrder/updatePurchase`;
 export const DELETE_MATERIALPO_URL = `${BASE_URL}/purchaseOrder/`;
 
