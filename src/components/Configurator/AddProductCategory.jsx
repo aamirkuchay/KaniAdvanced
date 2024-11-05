965a98a4bf1802d22221adbb74e0a831c51614d3
UMER MUKHTAR <shah.umer8493@gmail.com>
2024-11-05 14:48:46 +0530

    product category add view del

--- src/components/Configurator/AddProductCategory.jsx
+++ src/components/Configurator/AddProductCategory.jsx
@@ -1,76 +1,98 @@
 import React, { useState } from 'react';
 import DefaultLayout from '../../layout/DefaultLayout';
-import { ErrorMessage, Formik, useFormik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
 import * as Yup from 'yup';
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import ViewTable from './ViewTable';
+import Pagination from '../Pagination/Pagination';
+import useproductCategory from '../../hooks/useProductCategory';
 
 const AddProductCategory = () => {
-    const [selectedOption, setSelectedOption] = useState(null);
+    const {
+        productCategory,
+        edit,
+        currentproductCategory,
+        pagination,
+        handleDelete,
+        handleUpdate,
+        handleSubmit,
+        handlePageChange,
+    } = useproductCategory();
 
-    const formik = useFormik({
-        initialValues: {
-            productCategoryName: '',
-        },
-        validationSchema: Yup.object({
-            name: Yup.string().required('Name is required'),
-        }),
-        onSubmit: (values, { setSubmitting }) => {
-            if (!values.name.trim()) {
-                // If name field is empty
-                formik.setFieldError('name', 'Name cannot be empty');
-                setSubmitting(false);
-            } else {
-                // Otherwise, proceed with form submission
-                console.log(values);
-                // You can continue with your form submission logic here
-            }
-        },
-    });
+
 
     return (
         <DefaultLayout>
-            <Breadcrumb pageName="Product Category" />
-            <div>
-                <div className="flex flex-col gap-9">
-                    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                        <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-                            <h3 className="font-medium text-slate-500 text-center text-xl dark:text-white">
-                                Product Category
-                            </h3>
-                        </div>
-                        <form onSubmit={formik.handleSubmit} className="space-y-4">
-                            <div className="p-6.5">
-                                <div className="mb-4.5 flex flex-wrap gap-6">
-                                    <div className="flex-1 min-w-[300px]">
-                                        <label className="mb-2.5 block text-black dark:text-white">Product Catagory Name</label>
-                                        <input
-                                            type="text"
-                                            name="name"
-                                            placeholder="Add Product Catagory "
-                                            onChange={formik.handleChange}
-                                            onBlur={formik.handleBlur}
-                                            value={formik.values.name}
-                                            className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                                        />
-                                        {formik.touched.name && formik.errors.name ? (
-                                            <div className="text-red-600 text-sm">{formik.errors.name}</div>
-                                        ) : null}
-                                    </div>
+        <Breadcrumb pageName="Configurator/Add Product Category" />
+        <div>
+            <Formik
+                initialValues={currentproductCategory}
+                enableReinitialize={true}
+                validate={values => {
+                    const errors = {};
+                   
+                    if (!values.productCategoryName) {
+                        errors.productCategoryName = 'Required';
+                    }
+                    return errors;
+                }}
+                onSubmit={handleSubmit}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <div className="flex flex-col gap-9">
+                            {/* Form fields */}
+                            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                                <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+                                    <h3 className="font-medium text-slate-500 text-center text-xl dark:text-white">
+                                    {edit ? "UPDATE PRODUCT CATEGORY" : "ADD PRODUCT CATEGORY"}
+                                    </h3>
                                 </div>
-
-                                <button
-                                    type="submit"
-                                    className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-4"
-                                    disabled={formik.isSubmitting}
-                                >
-                                    Submit
-                                </button>
+                                <div className="p-6.5">
+                                    <div className="mb-4.5 flex flex-wrap gap-6">
+                                        <div className="flex-1 min-w-[300px]">
+                                            <label className="mb-2.5 block text-black dark:text-white"> Design Name</label>
+                                            <Field
+                                                type="text"
+                                                name="productCategoryName"
+                                                placeholder="Enter product Category Name"
+                                                className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-slate-700 dark:text-white dark:focus:border-primary"
+                                            />
+                                            <ErrorMessage name="productCategoryName" component="div" className="text-red-500" />
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-4">
+                                    {edit ? "UPDATE PRODUCT CATEGORY" : "CREATE PRODUCT CATEGORY"}
+                                    </button>
+                                </div>
                             </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </DefaultLayout>
+                            {!edit && (
+                             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                                 <div className="border-b border-stroke py-4 px-2 dark:border-strokedark">
+                                     <h3 className="font-medium text-slate-500 text-center text-xl dark:text-white">
+                                         <ViewTable
+                                             units={productCategory}
+                                             pagination={pagination}
+                                             totalItems={pagination.totalItems}
+                                             title={'Product Category'}
+                                             handleDelete={handleDelete}
+                                             handleUpdate={handleUpdate}
+                                         />
+                                         <Pagination
+                                             totalPages={pagination.totalPages}
+                                             currentPage={pagination.currentPage}
+                                             handlePageChange={handlePageChange}
+                                         />
+                                     </h3>
+                                 </div>
+                             </div>
+                         )}
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    </DefaultLayout>
     );
 };
 
